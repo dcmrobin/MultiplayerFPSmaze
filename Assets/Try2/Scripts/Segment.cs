59bd9e9e7ab86eefcec0236fbbbaff65826038de
DavidCM <dcm.robin@gmail.com>
2024-01-06 12:35:56 +0000

    okay try2 is looking promising

---
+++ Assets/Try2/Scripts/Segment.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Segment : MonoBehaviour
+{
+    public GameObject[] segmentPrefabList;
+    public Transform[] exits;
+
+    private void Start() {
+        int randomSegmentNum = Random.Range(0, segmentPrefabList.Length);
+
+        for (int j = 0; j < exits.Length; j++)
+        {
+            if (exits[j].childCount == 0)
+            {
+                GameObject newSegment = Instantiate(segmentPrefabList[randomSegmentNum], exits[j]);
+    
+                //newSegment.transform.rotation = Quaternion.Euler(0, exits[j].rotation.y, 0);
+            }
+        }
+    }
+}
