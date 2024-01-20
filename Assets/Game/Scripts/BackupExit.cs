69f2a73b7cfda5270527885c27123965674be92c
DavidCM <dcm.robin@gmail.com>
2024-01-20 13:26:36 +0000

    still trying

--- Assets/Game/Scripts/BackupExit.cs
+++ Assets/Game/Scripts/BackupExit.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class BackupExit : MonoBehaviour
+public class BackupExit : NetworkBehaviour
 {
     public GameObject wallPrefab;
 
--- Assets/Game/Scripts/SegmentOverlapCheck.cs
+++ Assets/Game/Scripts/SegmentOverlapCheck.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class SegmentOverlapCheck : MonoBehaviour
+public class SegmentOverlapCheck : NetworkBehaviour
 {
     public LayerMask overlapLayermask;
 
