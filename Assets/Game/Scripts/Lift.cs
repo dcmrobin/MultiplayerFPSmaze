680c4ca9d5cd372ffd26220d0f65ddffe46d5923
DavidCM <dcm.robin@gmail.com>
2024-01-25 10:46:07 +0000

    uhhh now the lift isn't spawning and is cutting off generation when it tries to I think

--- Assets/Game/Scripts/Lift.cs
+++ Assets/Game/Scripts/Lift.cs
@@ -7,21 +7,36 @@ public class Lift : NetworkBehaviour
 {
     public Transform liftGround;
     public NetworkVariable<bool> hasDescended = new NetworkVariable<bool>();
+    bool descending;
+    bool ascending;
 
     [ServerRpc(RequireOwnership = false)]
     public void ActivateLiftServerRpc()
     {
         if (!hasDescended.Value)
         {
-            //descend
+            ascending = false;
+            descending = true;
             hasDescended.Value = true;
             //can't go past -39.5 y value
         }
         else if (hasDescended.Value)
         {
-            //ascend
+            descending = false;
+            ascending = true;
             hasDescended.Value = false;
             //can't go past 0.5 y value
+        }
+    }
+
+    private void Update() {
+        if (descending && liftGround.position.y > -39.5)
+        {
+            liftGround.Translate(-liftGround.up);
+        }
+        else if (ascending && liftGround.position.y < 0.5)
+        {
+            liftGround.Translate(liftGround.up);
         }
     }
 }
